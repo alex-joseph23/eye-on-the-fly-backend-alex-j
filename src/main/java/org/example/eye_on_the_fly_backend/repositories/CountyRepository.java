62f2f008d84081948e0369f1733b042aa53d3e58
alex-joseph23 <alexandra.k.joseph2323@gmail.com>
2025-11-10 21:37:32 -0600

    County Repository

---
+++ src/main/java/org/example/eye_on_the_fly_backend/repositories/CountyRepository.java
@@ -0,0 +1,7 @@
+package org.example.eye_on_the_fly_backend.repositories;
+import org.example.eye_on_the_fly_backend.models.County;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface CountyRepository extends JpaRepository<County, Long> {
+}
